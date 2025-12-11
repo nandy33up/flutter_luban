2af93e32f96330941fb1ae439582b1df7f5492e3
ningbo.lv <lvningbo@126.com>
2025-12-11 17:23:37 +0800

    refactor(压缩): 重构图片压缩逻辑并简化API接口

--- lib/src/flutter_luban.dart
+++ lib/src/flutter_luban.dart
@@ -1,88 +1,58 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:isolate';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:image/image.dart';
 
+class CompressObject {
+  final Uint8List image;
+  final int quality;
+  final int step;
+  final bool autoRatio;
+  final double ignoreSize;
+
+  CompressObject({
+    required this.image,
+    this.quality = 85,
+    this.step = 10,
+    this.autoRatio = true,
+    this.ignoreSize = 200,
+  });
+}
+
+class CompressResult {
+  final Uint8List imageData;
+  final int width;
+  final int height;
+
+  CompressResult({
+    required this.imageData,
+    required this.width,
+    required this.height,
+  });
+}
+
 class Luban {
   Luban._();
 
-  static Future<String?> compressImage(CompressObject object) async {
-    if (kIsWeb) {
-      throw "Because the web does not support isolate, it does not support web image compression for the time being";
+  static Future<CompressResult> compressImage(CompressObject object) async {
+    return compute<CompressObject, CompressResult>(_compress, object);
+  }
+
+  static CompressResult _compress(CompressObject object) {
+    var bytes = object.image;
+    var imageSize = bytes.length / 1024;
+    if (imageSize <= object.ignoreSize) {
+      Image image = decodeImage(bytes)!;
+      return CompressResult(
+        imageData: bytes,
+        width: image.width,
+        height: image.height,
+      );
     }
-    return compute(_lubanCompress, object);
-  }
-
-  static Future<dynamic> compressImageQueue(CompressObject object) async {
-    final response = ReceivePort();
-    await Isolate.spawn(_lubanCompressQueue, response.sendPort);
-    final sendPort = await response.first;
-    final answer = ReceivePort();
-    sendPort.send([answer.sendPort, object]);
-    return answer.first;
-  }
-
-  static Future<List<String?>> compressImageList(List<CompressObject> objects) async {
-    return compute(_lubanCompressList, objects);
-  }
-
-  static void _lubanCompressQueue(SendPort port) {
-    final rPort = ReceivePort();
-    port.send(rPort.sendPort);
-    rPort.listen((message) {
-      final send = message[0] as SendPort;
-      final object = message[1] as CompressObject;
-      send.send(_lubanCompress(object));
-    });
-  }
-
-  static List<String?> _lubanCompressList(List<CompressObject> objects) {
-    var results = [];
-    objects.forEach((_o) {
-      results.add(_lubanCompress(_o));
-    });
-    return results as List<String?>;
-  }
-
-  static bool _parseType(String path, List<String> suffix) {
-    bool result = false;
-    for (int i = 0; i < suffix.length; i++) {
-      if (path.endsWith(suffix[i])) {
-        result = true;
-        break;
-      }
-    }
-    return result;
-  }
-
-  static String? _lubanCompress(CompressObject object) {
-    const List<String> jpgSuffix = ["jpg", "jpeg", "JPG", "JPEG"];
-    const List<String> pngSuffix = ["png", "PNG"];
-    bool isJpg = _parseType(object.imageFile.path, jpgSuffix);
-    bool isPng = false;
-
-    if (!isJpg) isPng = _parseType(object.imageFile.path, pngSuffix);
-    final originalFileName = object.imageFile.path.split("/").last;
-    File? newImageFile;
-    if (isJpg || isPng) {
-      newImageFile = File('${object.targetPath}/luban_$originalFileName.${isPng ? "png" : "jpg"}');
-    } else {
-      throw Exception("flutter_luban don't support this image type");
-    }
-    if (newImageFile.existsSync()) {
-      if (object.useCache) {
-        return newImageFile.path;
-      }
-      newImageFile.deleteSync();
-    }
-
-    Image image = decodeImage(object.imageFile.readAsBytesSync())!;
-    var length = object.imageFile.lengthSync();
+    Image image = decodeImage(bytes)!;
     bool isLandscape = false;
-    double size;
+    double targetSize;
     int fixelW = image.width;
     int fixelH = image.height;
     double thumbW = (fixelW % 2 == 1 ? fixelW + 1 : fixelW).toDouble();
@@ -98,208 +68,77 @@ class Luban {
     } else {
       scale = fixelW / fixelH;
     }
-
-    var imageSize = length / 1024;
     if (scale <= 1 && scale > 0.5625) {
       if (fixelH < 1664) {
-        size = (fixelW * fixelH) / pow(1664, 2) * 150;
-        size = size < 150 ? 150 : size;
+        targetSize = (fixelW * fixelH) / pow(1664, 2) * 150;
       } else if (fixelH >= 1664 && fixelH < 4990) {
         thumbW = fixelW / 2;
         thumbH = fixelH / 2;
-        size = (thumbH * thumbW) / pow(2495, 2) * 300;
-        size = size < 150 ? 150 : size;
+        targetSize = (thumbH * thumbW) / pow(2495, 2) * 300;
       } else if (fixelH >= 4990 && fixelH < 10240) {
         thumbW = fixelW / 4;
         thumbH = fixelH / 4;
-        size = (thumbW * thumbH) / pow(2560, 2) * 300;
-        size = size < 250 ? 250 : size;
+        targetSize = (thumbW * thumbH) / pow(2560, 2) * 300;
       } else {
         int multiple = fixelH / 1280 == 0 ? 1 : fixelH ~/ 1280;
         thumbW = fixelW / multiple;
         thumbH = fixelH / multiple;
-        size = (thumbW * thumbH) / pow(2560, 2) * 300;
-        size = size < 250 ? 250 : size;
+        targetSize = (thumbW * thumbH) / pow(2560, 2) * 300;
       }
     } else if (scale <= 0.5625 && scale >= 0.5) {
       int multiple = fixelH / 1280 == 0 ? 1 : fixelH ~/ 1280;
       thumbW = fixelW / multiple;
       thumbH = fixelH / multiple;
-      size = (thumbW * thumbH) / (1440.0 * 2560.0) * 200;
-      size = size < 250 ? 250 : size;
+      targetSize = (thumbW * thumbH) / (1440.0 * 2560.0) * 200;
     } else {
       int multiple = (fixelH / (1280.0 / scale)).ceil();
       thumbW = fixelW / multiple;
       thumbH = fixelH / multiple;
-      size = ((thumbW * thumbH) / (1280.0 * (1280 / scale))) * 500;
-      size = size < 250 ? 250 : size;
+      targetSize = ((thumbW * thumbH) / (1280.0 * (1280 / scale))) * 500;
     }
-    if ((imageSize < size) ||
-        (object.mode == CompressMode.LARGE2SMALL && object.ignoreSize > 0 && length / 1024 <= object.ignoreSize) ||
-        (object.mode == CompressMode.SMALL2LARGE && object.ignoreSize > 0 && length / 1024 >= object.ignoreSize)) {
-      newImageFile.writeAsBytesSync(encodeJpg(image, quality: object.quality));
-      return newImageFile.path;
+    targetSize = targetSize < object.ignoreSize ? object.ignoreSize : targetSize;
+    if (imageSize < targetSize) {
+      return CompressResult(
+        imageData: encodeJpg(image, quality: object.quality),
+        width: image.width,
+        height: image.height,
+      );
     }
-    Image oldImage;
     if (isLandscape) {
-      oldImage = copyResize(image, width: thumbH.toInt(), height: object.autoRatio ? null : thumbW.toInt());
+      image = copyResize(image, width: thumbH.toInt(), height: object.autoRatio ? null : thumbW.toInt());
     } else {
-      oldImage = copyResize(image, width: thumbW.toInt(), height: object.autoRatio ? null : thumbH.toInt());
+      image = copyResize(image, width: thumbW.toInt(), height: object.autoRatio ? null : thumbH.toInt());
     }
-
-    if (newImageFile.existsSync()) {
-      newImageFile.deleteSync();
-    }
-    if (object.mode == CompressMode.LARGE2SMALL) {
-      _large2SmallCompressImage(
-        image: oldImage,
-        file: newImageFile,
-        quality: object.quality,
-        targetSize: size,
-        step: object.step,
-        isJpg: isJpg,
-      );
-    } else {
-      _small2LargeCompressImage(
-        image: oldImage,
-        file: newImageFile,
-        quality: object.step,
-        targetSize: size,
-        step: object.step,
-        isJpg: isJpg,
-      );
-    }
-    return newImageFile.path;
+    var compressedData = _doCompress(
+      oldImage: image,
+      quality: object.quality,
+      targetSize: targetSize,
+      step: object.step,
+    );
+    return CompressResult(
+      imageData: compressedData,
+      width: image.width,
+      height: image.height,
+    );
   }
 
-  static _large2SmallCompressImage({
-    Image? image,
-    File? file,
+  static Uint8List _doCompress({
+    Image? oldImage,
     quality,
     targetSize,
     step,
-    bool isJpg = true,
   }) {
-    if (isJpg) {
-      var im = encodeJpg(image!, quality: quality);
-      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
-      if (tempImageSize / 1024 > targetSize && quality > step) {
-        quality -= step;
-        _large2SmallCompressImage(
-          image: image,
-          file: file,
-          quality: quality,
-          targetSize: targetSize,
-          step: step,
-        );
-        return;
-      }
-      file!.writeAsBytesSync(im);
-    } else {
-      _compressPng(
-        image: image!,
-        file: file,
+    var newImage = encodeJpg(oldImage!, quality: quality);
+    print('压缩后图片大小: ${newImage.lengthInBytes} 字节 (${(newImage.lengthInBytes / 1024).toStringAsFixed(2)} KB)');
+    if (newImage.lengthInBytes / 1024 > targetSize && quality > step) {
+      quality -= step;
+      return _doCompress(
+        oldImage: oldImage,
+        quality: quality,
         targetSize: targetSize,
-        large2Small: true,
+        step: step,
       );
     }
+    return newImage;
   }
-
-  static _small2LargeCompressImage({
-    Image? image,
-    File? file,
-    quality,
-    targetSize,
-    step,
-    bool isJpg = true,
-  }) {
-    if (isJpg) {
-      var im = encodeJpg(image!, quality: quality);
-      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
-      if (tempImageSize / 1024 < targetSize && quality <= 100) {
-        quality += step;
-        _small2LargeCompressImage(
-          image: image,
-          file: file,
-          quality: quality,
-          targetSize: targetSize,
-          step: step,
-          isJpg: isJpg,
-        );
-        return;
-      }
-      file!.writeAsBytesSync(im);
-    } else {
-      _compressPng(
-        image: image!,
-        file: file,
-        targetSize: targetSize,
-        large2Small: false,
-      );
-    }
-  }
-
-  ///level 1~9  level++ -> image--
-  static void _compressPng({
-    required Image image,
-    File? file,
-    level,
-    targetSize,
-    required bool large2Small,
-  }) {
-    var _level;
-    if (large2Small) {
-      _level = level ?? 1;
-    } else {
-      _level = level ?? 9;
-    }
-    List<int> im = encodePng(image, level: _level);
-    if (_level > 9 || _level < 1) {
-    } else {
-      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
-      if (tempImageSize / 1024 > targetSize) {
-        _compressPng(
-          image: image,
-          file: file,
-          targetSize: targetSize,
-          level: large2Small ? _level + 1 : _level - 1,
-          large2Small: large2Small,
-        );
-        return;
-      }
-    }
-
-    file!.writeAsBytesSync(im);
-  }
-}
-
-enum CompressMode {
-  SMALL2LARGE,
-  LARGE2SMALL,
-}
-
-class CompressObject {
-  final File imageFile;
-  final String targetPath;
-  final CompressMode mode;
-  final bool useCache;
-  final int quality;
-  final int step;
-
-  ///If you are not sure whether the image detail property is correct, set true, otherwise the compressed ratio may be incorrect
-  final bool autoRatio;
-
-  ///When the image size exceeds this threshold (in KB), no compression will be performed
-  final int ignoreSize;
-
-  CompressObject({
-    required this.imageFile,
-    required this.targetPath,
-    required this.mode,
-    this.useCache = false,
-    this.quality = 80,
-    this.step = 6,
-    this.autoRatio = true,
-    this.ignoreSize = 0,
-  });
 }
--- test/flutter_luban_test.dart
+++ test/flutter_luban_test.dart
@@ -1,8 +1,79 @@
-
-
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_luban/flutter_luban.dart';
+import 'package:image/image.dart';
+
+// 辅助函数：保存Uint8List到文件
+void saveImage(Uint8List imageData, String outputPath) {
+  final outputFile = File(outputPath);
+  outputFile.writeAsBytesSync(imageData);
+  print('图片已保存到: $outputPath');
+}
 
 void main() {
-  test('adds one to input values', () {
+  late File testImageFile;
+  late Uint8List testImage;
+  late int originalFileSize;
+  late Image originalImage;
+  late int originalWidth;
+  late int originalHeight;
+
+  setUp(() {
+    // 读取测试图片文件 - 使用JPG图片来测试质量参数的影响
+    testImageFile = File('screenshot/test.jpg');
+    testImage = testImageFile.readAsBytesSync();
+    originalFileSize = testImageFile.lengthSync();
+
+    // 解码原始图片获取宽高信息
+    originalImage = decodeImage(testImage)!;
+    originalWidth = originalImage.width;
+    originalHeight = originalImage.height;
+  });
+
+  test('compressImage should return Uint8List', () async {
+    // 打印原始文件信息
+    print('原始图片路径: ${testImageFile.path}');
+    print('原始图片大小: ${originalFileSize} 字节 (${(originalFileSize / 1024).toStringAsFixed(2)} KB)');
+    print('原始图片尺寸: ${originalWidth}x${originalHeight}');
+    print('-' * 50);
+
+    // 创建CompressObject
+    final compressObject = CompressObject(image: testImage);
+
+    // 调用compressImage方法
+    final CompressResult result = await Luban.compressImage(compressObject);
+
+    // 打印压缩结果信息
+    print(
+        '压缩后图片大小: ${result.imageData.lengthInBytes} 字节 (${(result.imageData.lengthInBytes / 1024).toStringAsFixed(2)} KB)');
+    print('压缩后图片尺寸: ${result.width}x${result.height}');
+    print('压缩比例: ${((1 - result.imageData.lengthInBytes / originalFileSize) * 100).toStringAsFixed(2)}%');
+    print('-' * 50);
+
+    // 打印前后变化对比
+    print('尺寸变化: ${originalWidth}x${originalHeight} -> ${result.width}x${result.height}');
+    print(
+        '大小变化: ${(originalFileSize / 1024).toStringAsFixed(2)} KB -> ${(result.imageData.lengthInBytes / 1024).toStringAsFixed(2)} KB');
+    print(
+        '节省空间: ${((originalFileSize - result.imageData.lengthInBytes) / 1024).toStringAsFixed(2)} KB (${((1 - result.imageData.lengthInBytes / originalFileSize) * 100).toStringAsFixed(2)}%)');
+
+    // 保存压缩后的图片
+    final outputPath = 'screenshot/compressed_test_${compressObject.quality}.jpg';
+    saveImage(result.imageData, outputPath);
+
+    // 验证结果类型
+    expect(result, isA<CompressResult>());
+    expect(result.imageData, isA<Uint8List>());
+
+    // 验证结果不为空
+    expect(result.imageData.isNotEmpty, true);
+
+    // 验证压缩后的大小小于原始大小
+    expect(result.imageData.lengthInBytes, lessThan(originalFileSize));
+
+    // 验证尺寸信息有效
+    expect(result.width, greaterThan(0));
+    expect(result.height, greaterThan(0));
   });
 }
